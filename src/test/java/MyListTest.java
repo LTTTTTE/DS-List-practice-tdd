2b60cc0383a4af0344427de6d173c368e246d0fd
DB2 <10100aaaaa@gmail.com>
2019-08-16 14:43:40 +0900

    Method add 추가, 테스트코드 추가
    - add 관련 테스트코드 추가
    - contains indexOf 등 메소드들 테스트코드 추가

--- src/test/java/MyListTest.java
+++ src/test/java/MyListTest.java
@@ -1,11 +1,15 @@
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class MyListTest {
 
     private MyList myList;
+
     @Before
     public void init(){
         myList = new MyList();
@@ -13,6 +17,7 @@ public class MyListTest {
         myList.add("40"); myList.add("50"); myList.add("60");
         myList.add("70"); myList.add("80"); myList.add("90");
     }
+
     @Test
     public void testInit(){
         MyList list = new MyList();
@@ -31,6 +36,30 @@ public class MyListTest {
     public void testEmptyClear(){
         myList.clear();
         assertThat(myList.isEmpty()).isTrue();
+    }
+
+    @Test
+    public void testIndexOf(){
+        assertThat(myList.indexOf("30")).isEqualTo(2);
+    }
+
+    @Test
+    public void testLastIndexOf(){
+        myList.add("30");
+        assertThat(myList.indexOf("30")).isNotEqualTo(myList.lastIndexOf("30"));
+    }
+
+    @Test
+    public void testContains(){
+        assertThat(myList.contains("30")).isTrue();
+    }
+
+    @Test
+    public void testContainsAll(){
+        List<String> anotherList = new ArrayList<>();
+        anotherList.add("10");anotherList.add("90");
+        anotherList.add("20");anotherList.add("80");
+        assertThat(myList.containsAll(anotherList)).isTrue();
     }
 
 }
